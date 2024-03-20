6585d76de77fe657df98ef8adf63be84b190f328
Bishal Shrestha <you@example.com>
2024-03-20 23:13:42 +0545

    added loading indioator

--- lib/module/home_page.dart
+++ lib/module/home_page.dart
@@ -18,54 +18,58 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: Colors.transparent,
-          systemOverlayStyle: SystemUiOverlayStyle.dark,
-          elevation: 0,
-          title: Column(
-            children: [
-              Text(
-                "Today",
-                style: GoogleFonts.ubuntu(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 30),
-              ),
-              Text(
-                formatDate(DateTime.now()),
-                style: GoogleFonts.ubuntu(
-                    color: Colors.grey,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 18),
-              )
-            ],
+    return Obx(
+      () => Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            backgroundColor: Colors.transparent,
+            systemOverlayStyle: SystemUiOverlayStyle.dark,
+            elevation: 0,
+            title: Column(
+              children: [
+                Text(
+                  "Today",
+                  style: GoogleFonts.ubuntu(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 30),
+                ),
+                Text(
+                  formatDate(DateTime.now()),
+                  style: GoogleFonts.ubuntu(
+                      color: Colors.grey,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 18),
+                )
+              ],
+            ),
           ),
-        ),
-        body: SizedBox.expand(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              WhetherItem(
-                icon: 'assets/loc.json',
-                text: "Your location is ",
-                subText: _controller.address,
-              ),
-              const SizedBox(height: 18),
-              WhetherItem(
-                icon: 'assets/temp.json',
-                text: "The temperature is ",
-                subText: "${_controller.temperature}°C",
-              ),
-              const SizedBox(height: 18),
-              WhetherItem(
-                icon: 'assets/thums_up.json',
-                text: "You should ",
-                subText: _controller.infoText.value,
-              ),
-            ],
-          ),
-        ));
+          body: _controller.isLoading.value
+              ? const Center(child: CircularProgressIndicator())
+              : SizedBox.expand(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      WhetherItem(
+                        icon: 'assets/loc.json',
+                        text: "Your location is ",
+                        subText: _controller.address,
+                      ),
+                      const SizedBox(height: 18),
+                      WhetherItem(
+                        icon: 'assets/temp.json',
+                        text: "The temperature is ",
+                        subText: "${_controller.temperature}°C",
+                      ),
+                      const SizedBox(height: 18),
+                      WhetherItem(
+                        icon: 'assets/thums_up.json',
+                        text: "You should ",
+                        subText: _controller.infoText.value,
+                      ),
+                    ],
+                  ),
+                )),
+    );
   }
 }
